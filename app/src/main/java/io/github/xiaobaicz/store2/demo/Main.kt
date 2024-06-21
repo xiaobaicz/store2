2b44307ff4079623dd78ef130b648a5e993be968
bocheng.lao <xiaojinjincz@outlook.com>
2024-06-21 15:24:23 +0800

    upgrade demo

--- app/src/main/java/io/github/xiaobaicz/store2/demo/Main.kt
+++ app/src/main/java/io/github/xiaobaicz/store2/demo/Main.kt
@@ -8,11 +8,20 @@ import io.github.xiaobaicz.store2.serializer.GsonSerializer
 
 fun main() {
     MMapSaver.dir = "."
+    // get table
     val local: Local by localStore
+    // check key
     println(localStore.has(Local::version))
+    // get
+    println(local.debug)
     println(local.version)
     println(local.user)
+    // set
     local.version = 110
+    // remove key
+    localStore.remove(Local::version)
+    // clear table
+    localStore.clear()
 }
 
 val storeFactory = Store.Factory()
@@ -22,6 +31,8 @@ val storeFactory = Store.Factory()
 val localStore = storeFactory.get<Local>()
 
 interface Local {
+
+    var debug: Boolean
 
     @IntDef(666)
     var version: Int
