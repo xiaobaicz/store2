0ee345c88105966ea68e7fcbb8d6823f9c0d72ac
bocheng.lao <xiaojinjincz@outlook.com>
2024-06-25 18:04:06 +0800

    comment

--- store2/src/main/java/io/github/xiaobaicz/store2/Saver.kt
+++ store2/src/main/java/io/github/xiaobaicz/store2/Saver.kt
@@ -2,14 +2,41 @@ package io.github.xiaobaicz.store2
 
 interface Saver {
 
+    /**
+     * field key exist?
+     * @param table target table
+     * @param key field key
+     * @return exist?
+     */
     fun has(table: String, key: String): Boolean
 
+    /**
+     * store field value
+     * @param table target table
+     * @param key field key
+     * @param value field value
+     */
     fun store(table: String, key: String, value: String)
 
+    /**
+     * restore field value
+     * @param table target table
+     * @param key field key
+     * @return field value
+     */
     fun restore(table: String, key: String): String
 
+    /**
+     * remove field
+     * @param table target table
+     * @param key field key
+     */
     fun remove(table: String, key: String)
 
+    /**
+     * clear table
+     * @param table target table
+     */
     fun clear(table: String)
 
 }
--- store2/src/main/java/io/github/xiaobaicz/store2/Serializer.kt
+++ store2/src/main/java/io/github/xiaobaicz/store2/Serializer.kt
@@ -4,8 +4,19 @@ import kotlin.reflect.KType
 
 interface Serializer {
 
+    /**
+     * serialization
+     * @param any target object
+     * @return serialization string
+     */
     fun serialization(any: Any): String
 
+    /**
+     * deserialization
+     * @param returnType target type
+     * @param value serialization string
+     * @return target object
+     */
     fun deserialization(returnType: KType, value: String): Any
 
 }
